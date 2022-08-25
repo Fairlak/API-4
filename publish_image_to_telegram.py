f2e26a7b3e20cc27aff067f4b96a05a7df6f80d4
Fairlak <Azarkevich.2005@mail.ru>
2022-08-25 19:45:06 +0300

    Moved load_dotenv inside def main

--- publish_image_to_telegram.py
+++ publish_image_to_telegram.py
@@ -2,31 +2,32 @@ import os
 import random
 import time
 
+
 import telegram
 from dotenv import load_dotenv
 
-load_dotenv()
-TELEGRAM_KEY = os.getenv("TELEGRAM_KEY")
-TIME = os.getenv("TIME")
-CHAT_ID = os.getenv("CHAT_ID")
 
 
-
-
-def publish_image():
-    bot = telegram.Bot(token=TELEGRAM_KEY)
+def publish_image(telegram_key, message_interval, chat_id):
+    bot = telegram.Bot(token=telegram_key)
     while True:
         dir_pict = ['EPIC_NASA_images', 'NASA_images', 'spacex_images']
         random_dir = random.choice(dir_pict)
         pictures = os.listdir(path=random_dir)
         for picture in pictures:
             path = f'{random_dir}/{picture}'
-            bot.send_document(chat_id=CHAT_ID, document=open(path, 'rb'))
-            time.sleep(int(TIME))
+            bot.send_document(chat_id=chat_id, document=open(path, 'rb'))
+            time.sleep(int(message_interval))
 
 
+def main():
+    load_dotenv()
+    telegram_key = os.getenv("TELEGRAM_KEY")
+    message_interval = os.getenv("TIME")
+    chat_id = os.getenv("CHAT_ID")
+    publish_image(telegram_key, message_interval, chat_id)
 
 if __name__ == '__main__':
-    publish_image()
+    main()
 
 
